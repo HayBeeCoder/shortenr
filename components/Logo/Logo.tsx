45cdbd2bbf6bbd3a6bb1af4291cacdbd811b035b
Oguntade Abass <54000678+HayBeeCoder@users.noreply.github.com>
2022-07-05 06:09:59 -0700

    Update Logo.tsx

--- components/Logo/Logo.tsx
+++ components/Logo/Logo.tsx
@@ -6,9 +6,9 @@ const Logo = () => {
             <span>
 
                 <svg width="32" height="48" viewBox="0 0 40 48" fill="none" xmlns="http://www.w3.org/2000/svg">
-                    <path d="M16 20V14C16 9.58172 19.5817 6 24 6V6C28.4183 6 32 9.58172 32 14V20" stroke="#0B1A30" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
-                    <path d="M24 32L24 16" stroke="#0047B3" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
-                    <path d="M16 28V34C16 38.4183 19.5817 42 24 42V42C28.4183 42 32 38.4183 32 34V28" stroke="#0B1A30" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
+                    <path d="M16 20V14C16 9.58172 19.5817 6 24 6V6C28.4183 6 32 9.58172 32 14V20" stroke="#0B1A30" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" />
+                    <path d="M24 32L24 16" stroke="#0047B3" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" />
+                    <path d="M16 28V34C16 38.4183 19.5817 42 24 42V42C28.4183 42 32 38.4183 32 34V28" stroke="#0B1A30" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" />
                 </svg>
             </span>
             <p className='text-2xl font-black tracking-wider '>
