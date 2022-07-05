c960d63ef195701e4e207c058143ddf6c9c082b9
Oguntade Abass <54000678+HayBeeCoder@users.noreply.github.com>
2022-07-05 13:43:56 -0700

    [fix] adjusts header

--- components/Layout/Layout.tsx
+++ components/Layout/Layout.tsx
@@ -1,3 +1,5 @@
+import Link from 'next/link'
+import { useRouter } from 'next/router'
 import React from 'react'
 import Button from '../Button'
 import Logo from '../Logo'
@@ -7,19 +9,31 @@ interface ILayout {
 }
 
 const Layout: React.FC<ILayout> = ({ children }) => {
+  const { pathname } = useRouter()
+  // console.log(router)
   return (
     <div className='w-ful'>
       <header className='fixed top-0  w-full px-2 py-3  flex justify-between gap-10'>
         <Logo />
-        <div className=' flex flex-auto   gap-1'>
-        
-          <Button classname=' basis-36 py-2 px-2 text-sm border-[1px] border-solid border-[#2b7fff] bg-[#2b7fff] inline'>
-            Log In
-          </Button>
-          <Button classname='basis-36 flex-grow py-2 px-2 text-sm text-[#2b7fff] border-[1px] border-solid border-[#2b7fff] bg-transparent inline'>
-            Sign Up
-          </Button>
-        </div>
+        {
+          (pathname == "/login" || pathname == "/signup")
+            ?
+            <></>
+            :
+            <div className=' flex flex-auto   gap-1'>
+              <Link href="/login">
+                <Button classname=' basis-36 py-2 px-2 text-sm border-[1px] border-solid border-[#2b7fff] bg-[#2b7fff] inline'>
+                  Log In
+                </Button>
+              </Link>
+              <Link href="/signup">
+                <Button classname='basis-36 flex-grow py-2 px-2 text-sm text-[#2b7fff] border-[1px] border-solid border-[#2b7fff] bg-transparent inline'>
+                  Sign Up
+                </Button>
+              </Link>
+            </div>
+
+        }
       </header>
       {children}
     </div>
---
+++ pages/login.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const login = () => {
+  return (
+    <p className='text-orange-400 font-bold text-xl'>login</p>
+  )
+}
+
+export default login
