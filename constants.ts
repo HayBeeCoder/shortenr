9b5bc84a23a779af33938a71d6e7d210c5100a12
Oguntade Abass <54000678+HayBeeCoder@users.noreply.github.com>
2022-07-08 01:46:42 -0700

    Update constants.ts

--- constants.ts
+++ constants.ts
@@ -1,4 +1,4 @@
-const BASE_URL = "http://134.122.124.219/v1/"
+const BASE_URL = "https://shortenr-api.herokuapp.com/v1/"
 const REGEX_EMAIL = RegExp(
     /^(([^<>()\[\]\.,;:\s@\"]+(\.[^<>()\[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i
 );  
