c3faf3a539f0ebc30e140aff9de121448b6a5ef6
Oguntade Abass <54000678+HayBeeCoder@users.noreply.github.com>
2022-07-23 01:38:41 -0700

    Update DateAnalytics.tsx

--- components/DateAnalytics/DateAnalytics.tsx
+++ components/DateAnalytics/DateAnalytics.tsx
@@ -10,78 +10,68 @@ interface IProps {
 }
 
 function getDayFromDate(date: string) {
-
   return parseInt(date.slice(date.length - 2));
 }
 function getMonthFromDate(date: string) {
   return parseInt(date.slice(6, 8));
 }
 
-
 //Each index of array signifies the month with January being 0 and December 11
 //Each array entry represents the number of days in the corresponding index ( month )
 // const MONTHS_NO_OF_DAYS = [31,28]
 
 const DateAnalytics = ({ date_analytics, isLoading }: IProps) => {
-  const [error,setError] = useState('')
-  const [dayLabel, setDayLabels] = useState<string[] | null>(null)
-  const [dayViewsCount,setDayViewsCount] = useState<number[] | null>(null)
+  const [error, setError] = useState("");
+  const [dayLabel, setDayLabels] = useState<string[] | null>(null);
+  const [dayViewsCount, setDayViewsCount] = useState<number[] | null>(null);
   // const [month, setMonth] = useState<Array<{ label: string; value: number }> | null >(null);
   // const [currentMonth, setCurrentMonth] = useState<any+>();
 
   useEffect(() => {
-    let day_count = 1
-   
+    let day_count = 1;
+
     // console.log(first_item)
     // console.log("The month is : " , length_of_month, " long.")
     // const month = getMonthFromDate(first_item.date)
     if (date_analytics.current_month && date_analytics.current_month[0]) {
-      let {current_month} = date_analytics
-        console.log("Current Month: ",  date_analytics.current_month)
-      let last_item = current_month[current_month.length - 1]
+      let { current_month } = date_analytics;
+      console.log("Current Month: ", date_analytics.current_month);
+      let last_item = current_month[current_month.length - 1];
       let first_item = date_analytics.current_month[0];
-      let day_of_last_item = getDayFromDate(last_item?.date as string)  // a string would be returnUrl
-      const length_of_month = dayjs(first_item.date).daysInMonth()
-      console.log(length_of_month)
+      let day_of_last_item = getDayFromDate(last_item?.date as string); // a string would be returnUrl
+      const length_of_month = dayjs(first_item.date).daysInMonth();
+      console.log(length_of_month);
 
-
-
-      //using below daysInMonth method seems to not work for some dates, 
+      //using below daysInMonth method seems to not work for some dates,
       //I gat my eyes on you though , we shall see
       //snaps finger
-      let dayLabelsArray = []
+      let dayLabelsArray = [];
       // const array_for_month = Array.from({length: length_of_month}, () => ({label: `Day ${first_day++}`,value: 0}))
       // const days_of_month = new Array(length_of_month).fill(((item,index) => `Day ${index + 1}`)())
-        while(day_count <= length_of_month){
-          dayLabelsArray.push(`Day ${day_count++}`)
+      while (day_count <= length_of_month) {
+        dayLabelsArray.push(`Day ${day_count++}`);
+      }
+      console.log(dayLabelsArray);
+      setDayLabels(dayLabelsArray);
 
-
-        }
-        console.log(dayLabelsArray)
-      setDayLabels(dayLabelsArray)
-
-      const views_of_month = new Array(day_of_last_item).fill(0)
+      const views_of_month = new Array(day_of_last_item).fill(0);
       // const {current_month } = date_analytics
-      for(let i = 0 ; i < current_month.length; i++){
-        let item = current_month[i]
-        const day = getDayFromDate(item.date)
+      for (let i = 0; i < current_month.length; i++) {
+        let item = current_month[i];
+        const day = getDayFromDate(item.date);
         // console.log("the day is : " , day)
         // setDayViewsCount((dayViewsCount) => {
-          // let a = dayViewsCount as number[]
-          views_of_month[day - 1 ] = item.count__sum
-          // return a
+        // let a = dayViewsCount as number[]
+        views_of_month[day - 1] = item.count__sum;
+        // return a
         // } )
       }
-      setDayViewsCount(views_of_month)
-      
+      setDayViewsCount(views_of_month);
     }
   }, [date_analytics]);
 
-  
-
-
-    console.log("days:  " , dayLabel)
-    console.log("views:  " , dayViewsCount)
+  console.log("days:  ", dayLabel);
+  console.log("views:  ", dayViewsCount);
   // const [doesCountriesExist ] = useState(false);
   // const [browsersData, setB] = useState(b);
 
@@ -141,7 +131,7 @@ const DateAnalytics = ({ date_analytics, isLoading }: IProps) => {
     //   console.log(canvasElement)
     if (dayLabel && dayViewsCount) {
       // console.log("oguntade abass omowale ")
-      // let does_real_values_exist = 
+      // let does_real_values_exist =
       // console.log(date_analytics.current_month);
       // const [labels, values] = processOtherAnalytics(
       //   date_analytics.
@@ -151,66 +141,55 @@ const DateAnalytics = ({ date_analytics, isLoading }: IProps) => {
       //   setBrowsersDataExist(true);
       //   const backgroundColor = COLORS.slice(0, values.length + 1);
 
-        const chartType: ChartType = "line";
-        // const axes = true;
+      const chartType: ChartType = "line";
+      // const axes = true;
       //   const legend = true;
-        const config = {
-          canvasElement,
-          chartType,
-          labels: dayLabel,
-          data: dayViewsCount,
-          backgroundColor: [],
-          borderColor: 'green',
-          axes: true,
-          legend: false,
-        };
-        buildChart(config, 5);
+      const config = {
+        canvasElement,
+        chartType,
+        labels: dayLabel,
+        data: dayViewsCount,
+        backgroundColor: [],
+        borderColor: "green",
+        axes: true,
+        legend: false,
+      };
+      buildChart(config, 5);
       // } else setBrowsersDataExist(false);
     }
     // }
-  }, [dayLabel,dayViewsCount,date_analytics]);
+  }, [dayLabel, dayViewsCount, date_analytics]);
 
   useEffect(() => {
-    try{
-
-      // monthChart();
-    }catch(e: any){
-      setError(e)
+    try {
+      monthChart();
+    } catch (e: any) {
+      setError(e);
     }
 
     // return () => {
     //   second
     // }
-  }, [monthChart,date_analytics]);
+  }, [monthChart, date_analytics]);
 
   return (
     <>
-    <div>
-      {error}
-    </div>
-    <SubAnalytic
-      title="Current Month Views"
-      toolTipMessage="Visits count within current month"
-      special
-      should_flex
-    >
-      <div className="px-4  h-[300px] md:h-[250px]  ">
-        {
-            JSON.stringify(date_analytics.current_month) !== '[]' ?
-
-        // <canvas
-        //   id="dateChart"
-        //   className=" md:h-full w-full"
-        // ></canvas>
-        <p> hello! </p>
-        :
-            
-        <p className="text-center text-sm italic">No data yet!</p>
-
-      }
-      </div>
-    </SubAnalytic>
-        </>
+      <div>{error}</div>
+      <SubAnalytic
+        title="Current Month Views"
+        toolTipMessage="Visits count within current month"
+        special
+        should_flex
+      >
+        <div className="px-4  h-[300px] md:h-[250px]  ">
+          {JSON.stringify(date_analytics.current_month) !== "[]" ? (
+            <canvas id="dateChart" className=" md:h-full w-full"></canvas>
+          ) : (
+            <p className="text-center text-sm italic">No data yet!</p>
+          )}
+        </div>
+      </SubAnalytic>
+    </>
   );
 };
 
