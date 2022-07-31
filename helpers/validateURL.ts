3c6d6421846ae6a768845a40e7252953b65a1893
Oguntade Abass <54000678+HayBeeCoder@users.noreply.github.com>
2022-07-31 14:44:44 +0100

    Update validateURL.ts

--- helpers/validateURL.ts
+++ helpers/validateURL.ts
@@ -10,13 +10,14 @@ export const validateURL = (url: string): ReturnValue => {
   // console.log(url)
   // try {
     let a = /^(http:\/\/www\.|https:\/\/www\.|http:\/\/|https:\/\/)?[a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,5}(:[0-9]{1,5})?(\/.*)?$/gm.test(url.trim());
+    
     // let a = REGEX_URL.test(url.trim());
     // let a = REGEX_URL.test(url.trim());
 
     // console.log("aaaaaaaaaaa:" ,a)
     // console.log('regex: ', REGEX_URL)
     const returnValues = {
-      regex_test: a,
+      regex_test: a && !url.includes(" "),
       url_contains_domain_test: url.includes(SERVER_DOMAIN)
     }
     // returnValue = returnValues;
